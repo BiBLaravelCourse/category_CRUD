be455d5e82b733adea10becb08815311c6149e71
Zaw Htet Aung <scm.zawhtetaung.ece@gmail.com>
2022-07-12 15:06:23 +0630

    Navbar Active Status

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,31 +8,32 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+                    <a class="nav-link @if(request()->url() == route('home')) active @endif" aria-current="page" href="{{route('home')}}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.index')}}">Posts</a>
+                    <a class="nav-link @if(request()->url() == route('posts.index')) active @endif" href="{{route('posts.index')}}">Posts</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('categories.index')}}">Categories</a>
+                    <a class="nav-link @if(request()->url() == route('categories.index')) active @endif" href="{{route('categories.index')}}">Categories</a>
                 </li>
             </ul>
 
+            <!-- Right-Side Nav -->
             <ul class="navbar-nav mb-2 mb-lg-0">
-
                 @if(Auth::check())
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.create')}}">Create A Post</a>
+                    <a class="nav-link @if(request()->url() == route('posts.create')) active @endif" href="{{route('posts.create')}}">Create A Post</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('my-posts.index')}}">My Post</a>
+                    <a class="nav-link @if(request()->url() == route('my-posts.index')) active @endif" href="{{route('my-posts.index')}}">My Post</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link fw-bold dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link fw-bold dropdown-toggle @if(request()->url() == route('profile',Auth::id())) active @endif" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         {{ Auth::user()->name }}
+                        <img src="/storage/images/profile_avater_small.png" class="img-fluid ms-1" style="width:32px; height:32px;" alt="Profile Avater">
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="{{route('profile', Auth::id() )}}">Profile</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
